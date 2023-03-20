e0b2bb8880403c68010f0614f48db3b111d6166b
Hupperich-Manuel <mahupperich@gmail.com>
2023-03-20 21:06:52 +0100

    deleted alb_dns_name duplicate

--- services/webserver-cluster/02_outputs.tf
+++ services/webserver-cluster/02_outputs.tf
@@ -18,8 +18,3 @@ output "asg_name" {
   value = aws_autoscaling_group.example.name
   description = "The name of the auto Scaling Group"
 }
-
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
