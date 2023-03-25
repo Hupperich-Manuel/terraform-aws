c10b0179ab95bb17abdee8c951c806d371eb481d
Hupperich-Manuel <mahupperich@gmail.com>
2023-03-25 12:15:10 +0100

    Adapted to the code following the guidelines of the tutorial

--- services/webserver-cluster/01_main.tf
+++ services/webserver-cluster/01_main.tf
@@ -72,22 +72,27 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
-  
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
   # Allow inbound HTTP requets
-  ingress {
     from_port = local.http_port
     to_port = local.http_port
     protocol = local.tcp_protocol
     cidr_blocks = local.all_ips
   }
 
+resource "aws_security_group_rule" "allow_http_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
   # Allow all outbound requests
-  egress {
+
     from_port = local.any_port
     to_port = local.any_port
     protocol = local.any_protocol
     cidr_blocks = local.all_ips
-  }
 }
 
 resource "aws_lb_target_group" "asg" {
@@ -138,4 +143,15 @@ locals {
   any_protocol = "-1"
   tcp_protocol = "tcp"
   all_ips = ["0.0.0.0/0"]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
--- services/webserver-cluster/02_outputs.tf
+++ services/webserver-cluster/02_outputs.tf
@@ -1,15 +1,3 @@
-
-# data "aws_vpc" "default" {
-  #default = true
-#}
-
-# data "aws_subnets" "default" {
-  #filter {
-   # name = "vpc-id"
-    #values = [data.aws_vpc.default.id]
-  #}
-#}
-
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
